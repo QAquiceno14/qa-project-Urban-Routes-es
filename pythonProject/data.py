0c8f3af6318169ae6e674268941e105f61ca8fbe
adriana <adriana.quiceno.017@gmail.com>
2024-10-27 11:28:25 -0500

    Entrega para revision

--- pythonProject/data.py
+++ pythonProject/data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-1ac3734e-d92b-4b28-8405-677dfc5d9130.containerhub.tripleten-services.com/?lng=es'
+urban_routes_url = 'https://cnt-a7f8acb2-a8d3-4458-8f98-8a009cf6d553.containerhub.tripleten-services.com/?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
